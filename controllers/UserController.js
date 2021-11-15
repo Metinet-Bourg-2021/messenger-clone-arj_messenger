2ebb9f7dee33757909fe088e1da2381e0b80c443
moussare <45038815+moussare@users.noreply.github.com>
2021-11-15 12:03:47 +0100

    authenticate fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,22 +6,9 @@ const jwt = require('jsonwebtoken');
 async function authenticate({username, password}, callback)
 {
     try{
-        const hash = await bcrypt.hash(password,10)
         const userFind = await User.findOne({username:username})
-
         if(!userFind){
-            let user = new User({
-                username: username,
-                password: hash,
-                picture_url:picture.getRandomURL(),
-            });
-            const token = await jwt.sign({userId: user._id}, 'secret_key');
-            user.token = token
-            const userSave = await user.save(user)
-            if(!userSave){
-                return callback({code:"NOT_AUTHENTICATED", data:{}});
-            }
-            return callback({code:"SUCCESS", data:{"username":userSave.username,"token":userSave.token,"picture_url":userSave.picture_url}});
+            await save({username, password}, callback)
         }else{
             let isValid = await bcrypt.compare(password,userFind.password)
             if(isValid) return callback({code:"SUCCESS", data:{"username":userFind.username,"token":userFind.token,"picture_url":userFind.picture_url}});
@@ -32,27 +19,21 @@ async function authenticate({username, password}, callback)
     }
 }
 
-async function login(req, res)
-
+async function save({username, password}, callback)
 {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (user === null)
-            {
-                return res.status(404).json({ error: 'User not found !' });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid)
-                    {
-                        return res.status(401).json({ error: 'Wrong password !' });
-                    }
-                    const token = jwt.sign({userId: user._id}, 'secret_key', {expiresIn: "1h"});
-                    res.status(200).json({userId: user._id, token: token});
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+    const hash = await bcrypt.hash(password,10)
+    let user = new User({
+        username: username,
+        password: hash,
+        picture_url:picture.getRandomURL(),
+    });
+    const token = await jwt.sign({userId: user._id}, 'secret_key');
+    user.token = token
+    const userSave = await user.save(user)
+    if(!userSave){
+        return callback({code:"NOT_AUTHENTICATED", data:{}});
+    }
+    return callback({code:"SUCCESS", data:{"username":userSave.username,"token":userSave.token,"picture_url":userSave.picture_url}});
 }
 
-module.exports = {authenticate: authenticate, login: login};
+module.exports = {authenticate: authenticate};
