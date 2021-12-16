495e332a818cde950aefc415916393eff4364b79
AmineFajry <74823891+AmineFajry@users.noreply.github.com>
2021-12-16 08:48:06 +0100

    reaction

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -146,4 +146,44 @@ async function postMessage({token, conversation_id, content,sockets,io}, callbac
         console.log(err)
     }
 }
-module.exports = {deleteMessage: deleteMessage,updateMessage:updateMessage,postMessage:postMessage,replyMessage:replyMessage};
+
+async function reactMessage({token, conversation_id, message_id, reaction,sockets}, callback)
+{
+
+    try{
+        let isValid = await userCtr.tokenIsValid(token)
+        if(!isValid) return callback({code:"NOT_FOUND_USER", data:{}});
+        const userConnected = await User.findOne({token:token})
+        const conversationFind = await  Conversation.findOne({id:conversation_id});
+        const message = conversationFind.messages.find(_message => _message.id === message_id)
+        const messageIndex = conversationFind.messages.findIndex(_message => _message.id === message_id)
+
+        message.reactions[userConnected.username] = reaction
+
+        conversationFind.messages[messageIndex] = message
+        conversationFind.markModified('message')
+        message.markModified('reactions')
+
+        let conversationSave = await conversationFind.save();
+        let messageSave = await message.save();
+
+        console.log(conversationSave.messages.find(_message=>_message.id === 75))
+
+        if(sockets.length > 0)
+        {
+            sockets.forEach((socket)=>{
+                if(conversationFind.participants.includes(socket.username)){
+                    socket.client.emit('@messageReacted',{
+                        conversation_id,
+                        message:messageSave
+                    })
+                }
+            })
+
+            return callback({code:"SUCCESS", data:{}});
+        }
+    }catch (err){
+        console.log(err)
+    }
+}
+module.exports = {deleteMessage: deleteMessage,updateMessage:updateMessage,postMessage:postMessage,replyMessage:replyMessage,reactMessage:reactMessage};
